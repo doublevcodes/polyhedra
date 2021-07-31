37ed734313101f82eb0a80af708b212fbcf9789a
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-07-31 23:41:58 +0100

    Update and rename scenes/title.py to polyhedra/scene.py

--- polyhedra/scene.py
+++ polyhedra/scene.py
@@ -11,13 +11,11 @@ class Main(ThreeDScene):
         e_expl = Text("Number of edges").set_color(BLUE)
         v_expl = Text("Number of vertices").set_color(YELLOW)
         
-        
         # Animation - introduce the formula
         self.play(Write(title_text))
         self.play(Unwrite(title_text))
         self.play(Write(formula))
-        self.play(formula.animate.move_to(2.1 * UP))
-        self.play(formula.animate.scale(3))
+        self.play(formula.animate.move_to(2.1 * UP).scale(3))
 
         # Animation - labels for the formula
         for idx, clr, obj, pos in zip((0, 2, 4), (RED, BLUE, YELLOW), (f_expl, e_expl, v_expl), (-2, -1, 0.05)):
@@ -26,10 +24,4 @@ class Main(ThreeDScene):
             self.play(obj.animate.move_to(pos * UP))
 
         self.play(FadeOut(f_expl, v_expl, e_expl))
-        
-        formula_copy = formula.copy().scale(0.3333).to_corner(UL)
-        self.play(Transform(formula, formula_copy))
-        self.remove(formula)
-        formula = formula_copy.copy()
-        self.add_fixed_in_frame_mobjects(formula)
-        self.remove(formula_copy)
+        self.play(formula.animate.scale(0.3333).to_corner(UL)
